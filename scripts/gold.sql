db5c793958b6e7ead1db950f5061c4f4591f832f
Yashika Chandel <99817880+yashika3013@users.noreply.github.com>
2025-09-11 18:31:19 +0530

    Create gold.sql

---
+++ scripts/gold.sql
@@ -0,0 +1,58 @@
+
+CREATE VIEW gold.dim_customers AS 
+SELECT 
+	ROW_NUMBER()OVER(order by cst_id) as customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry as country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		 ELSE COALESCE(ca.gen,'n/a') 
+		 END as gender,
+	ca.bdate as birth_date,
+	ci.cst_create_date  AS create_date
+FROM silver.crm_cust_info ci 
+LEFT JOIN silver.erp_cust_az12 ca
+on		  ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+on		  ci.cst_key = la.cid;
+
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER()OVER(order by pn.prd_start_dt,pn.prd_key) as product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS product_start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL;-- Filter out all historical data
+
+
+
+CREATE VIEW gold.fact_sales AS
+SELECT 
+sd.sls_ord_num AS order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS ship_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
+
